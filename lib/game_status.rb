0c27777d266e98192c5bd0cbfd75cc0550e493fb
Andrew P <andrewphaviseth@gmail.com>
2020-04-30 23:47:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,32 +30,33 @@ def won?(board)
      position_2 = board[win_combination[1]]
      position_3 = board[win_combination[2]]
 
-     if position_1 == "X"  && position_2 == "X" && position_3 == "X" ||
-        position_1 == "0"  && position_2 == "O" && position_3 == "O"
+     if (position_1 == "X"  && position_2 == "X" && position_3 == "X" ||
+         position_1 == "0"  && position_2 == "O" && position_3 == "O")
+
         return win_combination
-        return true
+        won = true
      end
    end
-   win_combination
+  won
 end
 
-
-def full?(board)
-  board.none? do |marker|
-    marker == "X" || marker == "O"
-  end
-end
-
-def draw?(board)
-  !won?(board) && full?(board)
-end
-
-def over?(board)
-  won?(board) || draw?(board)
-end
-
-def winner(board)
-  if win_array = won?(board)
-    board[win_array.first]
-  end
-end
+#
+# def full?(board)
+#   board.none? do |marker|
+#     marker == "X" || marker == "O"
+#   end
+# end
+#
+# def draw?(board)
+#   !won?(board) && full?(board)
+# end
+#
+# def over?(board)
+#   won?(board) || draw?(board)
+# end
+#
+# def winner(board)
+#   if win_array = won?(board)
+#     board[win_array.first]
+#   end
+# end
