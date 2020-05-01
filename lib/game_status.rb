4dac61398a4b3e85966d6d45c740dc7f3b396e03
Andrew P <andrewphaviseth@gmail.com>
2020-05-01 01:02:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,16 +37,17 @@ def won?(board)
        if (position_1 == "X"  && position_2 == "X" && position_3 == "X") ||
           (position_1 == "O"  && position_2 == "O" && position_3 == "O")
           return win_combination
+          return true 
        else
-          false
+          return false
        end
     end
 end
 
-
-def full?(board)
-  board.none?{|marker| marker = " "}
-end
+# 
+# def full?(board)
+#   board.none?{|marker| marker = " "}
+# end
 
 def draw?(board)
   !won?(board) && full?(board)
