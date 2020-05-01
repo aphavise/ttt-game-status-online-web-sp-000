46fa29b965887faae974517bc90ec726b1426ec0
Andrew P <andrewphaviseth@gmail.com>
2020-05-01 00:01:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,8 +25,8 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   # binding.pry
- won_check = false
-  WIN_COMBINATIONS.each do |win_combination|
+
+   WIN_COMBINATIONS.each do |win_combination|
 
         first_index = win_combination[0]
         second_index = win_combination[1]
@@ -37,13 +37,13 @@ def won?(board)
         position_3 = board[third_index]
 
 
-     if (position_1 == "X"  && position_2 == "X" && position_3 == "X") ||
-        (position_1 == "0"  && position_2 == "O" && position_3 == "O")
-        return win_combination
-        won_check = true
-     end
-   end
-  won_check
+       if (position_1 == "X"  && position_2 == "X" && position_3 == "X") ||
+          (position_1 == "0"  && position_2 == "O" && position_3 == "O")
+          return win_combination
+          won = true
+       end
+    end
+  won
 end
 
 #
