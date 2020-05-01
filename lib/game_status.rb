05185011630815a4f4bae674cddf644cc819a1cf
Andrew P <andrewphaviseth@gmail.com>
2020-05-01 01:02:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,7 +37,6 @@ def won?(board)
        if (position_1 == "X"  && position_2 == "X" && position_3 == "X") ||
           (position_1 == "O"  && position_2 == "O" && position_3 == "O")
           return win_combination
-          return true
        else
           return false
        end
